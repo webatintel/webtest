f99d50cbaf89adef5fdacb6066d7a7439c56440d
Yunfei Hao <yunfei.hao@intel.com>
2020-12-03 14:53:44 +0800

    Simplify report format and add simd thread for wasm testing

--- src/gen_single_report.js
+++ src/gen_single_report.js
@@ -7,6 +7,7 @@ const os = require('os');
 const { table } = require('console');
 const settings = require('../config.json');
 const http = require('http');
+let backends = [];
 // const sendMail = require('./send_mail.js');
 
 function drawResultHeader(basedResult) {
@@ -22,31 +23,51 @@ function drawResultHeader(basedResult) {
   return tableHeader + '</tr>';
 }
 
-function drawRoundsHeader(basedResult) {
+function drawRoundsHeader(basedResult, buffer) {
   let basedRoundCol = "<tr>";
-  const basedResultLength = basedResult.test_rounds.length;
-  for (let i = 0; i < basedResultLength; i++) {
-    basedRoundCol += `<th>Round ${i + 1}</th>`;
+  let keys = Object.keys(buffer);
+  for (let key of keys) {
+    const basedResultLength = Object.keys(buffer[key]).length;
+    for (let i = 0; i < basedResultLength; i++) {
+      let backendName = Object.keys(buffer[key])[i];
+      if (!backends.includes(backendName)) backends.push(backendName);
+    }
   }
+  backends.sort();
+  basedRoundCol += backends.reduce((acc, cur) => {return acc + `<th>${cur}</th>`}, '');
 
-  let header = `<tr><th rowspan='2'>Workloads</th><th colspan='${basedResultLength}'>\
-    ${basedResult.device_info.CPU.info + " " + basedResult.device_info.Browser}</th>`;
+  let header = `<tr><th rowspan='2'>Workloads</th><th colspan='${backends.length}'>\
+    ${basedResult.device_info.CPU.info + " " + basedResult.device_info.Browser}</th></tr>`;
   header = header  + basedRoundCol + "</tr>";
   return header;
 }
 
-function drawRoundsResult(basedResult) {
-  let basedResultCol = `<tr><td>${basedResult.workload}</td>`;
+function drawRoundsResult(basedResult, buffer) {
+  let workloadLenth = Object.keys(buffer).length;
   const selectedStyle = "style='background-color: #4CAF50;'";
-  for (let i = 0; i < basedResult.test_rounds.length; i++) {
-    if (basedResult.test_rounds[i] !== undefined) {
-      if (i === basedResult.selected_round)
-        basedResultCol += `<td ${selectedStyle}>${basedResult.test_rounds[i].scores["Total Score"]}</td>`;
-      else
-        basedResultCol += `<td>${basedResult.test_rounds[i].scores["Total Score"]}</td>`;
-    } else {
-      basedResultCol += "<td> - </td>";
+  const goodStyle = "style='color:green'";
+  const badStyle = "style='color:red'";
+  let style = '';
+  let basedResultCol = '';
+  for (let i = 0; i < workloadLenth; i++){
+    basedResultCol += `<tr><td>${Object.keys(buffer)[i]}</td>`;
+    let backendsResult = buffer[Object.keys(buffer)[i]];
+    let wasmCol = `<td> - </td>`;
+    let webglCol = `<td> - </td>`;
+    let webgpuCol = `<td> - </td>`;
+    let wasmValue = backendsResult.hasOwnProperty('wasm') ? backendsResult['wasm']['Total Score'].replace('ms', '') : 0;
+    let webglValue = backendsResult.hasOwnProperty('webgl') ? backendsResult['webgl']['Total Score'].replace('ms', '') : 0;
+    let webgpuValue = backendsResult.hasOwnProperty('webgpu') ? backendsResult['webgpu']['Total Score'].replace('ms', '') : 0;
+    console.log(`${wasmValue}, ${webglValue}, ${webgpuValue}`);
+    if (webgpuValue - wasmValue > 0) {
+      style = goodStyle;
     }
+    style = webgpuValue - wasmValue > 0 ? goodStyle : badStyle;
+    wasmCol = `<td>${wasmValue}ms <span ${style}>${parseFloat((webgpuValue - wasmValue)/webgpuValue*100).toFixed(2)}%</span></td>`
+    style = webgpuValue - webglValue > 0 ? goodStyle : badStyle;
+    webglCol = `<td>${webglValue}ms <span ${style}>${parseFloat((webgpuValue - webglValue)/webgpuValue*100).toFixed(2)}%</span></td>`
+    webgpuCol = `<td>${webgpuValue}ms</td>`
+    basedResultCol += wasmCol + webglCol + webgpuCol;
   }
 
   const resultCol = basedResultCol + "</tr>";
@@ -119,6 +140,9 @@ async function genSingleTestReport(resultPaths) {
   let roundsTable = "<table>";
   let basedResult;
   let flag = false;
+  let buffer = {};
+  let length = Object.keys(resultPaths).length;
+  let n = 0;
   const commitId = await getCommitId();
   for (const key in resultPaths) {
     const resultPath = resultPaths[key];
@@ -131,21 +155,35 @@ async function genSingleTestReport(resultPaths) {
       basedResult = JSON.parse(rawData);
       console.log("based result: ", basedResult);
     }
-    // Draw result table
-    if (!flag) {
-      roundsTable += drawRoundsHeader(basedResult);
+
+    let modelName, backendName;
+    if (basedResult.workload.indexOf('MobileNet_Image') !== -1) modelName = 'MobileNet_Image';
+    if (basedResult.workload.indexOf('MobileNet_Tensor') !== -1) modelName = 'MobileNet_Tensor';
+    if (basedResult.workload.indexOf('ResNet_Image') !== -1) modelName = 'ResNet_Image';
+    if (basedResult.workload.indexOf('ResNet_Tensor') !== -1) modelName = 'ResNet_Tensor';
+
+    if (buffer[modelName] === undefined) {
+      buffer[modelName] = {};
     }
-    const resultTable = drawResultTable(basedResult);
-    resultTables += `${resultTable}<br>`;
-    roundsTable += drawRoundsResult(basedResult);
-    flag = true;
+    backendName = basedResult.test_result.backend;
+    if (buffer[modelName][backendName] === undefined) buffer[modelName][backendName] = {};
+    buffer[modelName][backendName] = basedResult.test_rounds[basedResult.selected_round].scores;
+    n++;
+
+    // Draw result table
+    console.log(`n is ${n}, length is ${length}`)
+    if (n === length) {
+    console.log(buffer);
+      roundsTable += drawRoundsHeader(basedResult, buffer);
+      const resultTable = drawResultTable(basedResult);
+      resultTables += `${resultTable}<br>`;
+      roundsTable += drawRoundsResult(basedResult, buffer);
+    }
   }
   roundsTable += "</table><br><br>";
 
   let workloadUrls = "<b>Workload Urls:</b> <br>";
-  for (let workload of settings.workloads) {
-    workloadUrls += `    - <b>${workload.name}</b>: <a href="${workload.url}">${workload.url}</a><br>`;
-  }
+  workloadUrls += `    - <a href="${settings.workloads[0].url}">${settings.workloads[0].url}</a><br>`;
   const chromePath = "<br><b>Chrome path: </b>" + settings.chrome_path;
   const chromeFlags = "<br><b>Chrome flags: </b>" + basedResult.chrome_flags;
   const commitIdHtml = "<br><b>TFJS repo commit id: </b>" + commitId;
@@ -160,9 +198,8 @@ async function genSingleTestReport(resultPaths) {
 	  th {background-color: #0071c5; color: #ffffff; font-weight: normal;} \
     </style>";
 
-  // Composite html body
-  const html = htmlStyle + roundsTable + "<b>Details:</b>"
-    + resultTables + "<br><br>" + workloadUrls + chromePath + chromeFlags + commitIdHtml + "<br><br><b>Device Info:</b>" + deviceInfoTable;
+  const html = htmlStyle + roundsTable
+    + "<br><br>" + workloadUrls + chromePath + chromeFlags + commitIdHtml + "<br><br><b>Device Info:</b>" + deviceInfoTable;
   console.log("******Generate html to test.html******");
   await fsPromises.writeFile('./test.html', html);
   return Promise.resolve(html);
